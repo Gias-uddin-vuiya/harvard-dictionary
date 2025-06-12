007a8973cc995f67b7fdf161235e2935c312d807
Gias-uddin-vuiya <giasuddinvuiya2022@gmail.com>
2025-06-12 15:56:59 +0600

    add layout, home pages and connected with app

---
+++ app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, flash, redirect, render_template, session, request
+from flask_session import Session
+# configure application
+app = Flask(__name__)
+
+# configue session to ensure filesystem (insted of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+
+# Ensuser that users get up to date data
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+def home():
+
+    return render_template("home.html")
---
+++ project.py
@@ -0,0 +1,15 @@
+def main():
+    print("This is the main function of the project module.")
+
+
+def fetch_definition(word):
+    # Call Free Dictionary API
+    pass
+
+def translate_bengali(word):
+    # Call LibreTranslate API
+    pass
+
+def mark_word(word, status, memory):
+    # Mark as memorized or ignored
+    pass
